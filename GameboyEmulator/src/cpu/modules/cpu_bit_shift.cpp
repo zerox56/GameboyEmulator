c731ed8457d755fdc06042c0d53f67fadf60d771
zerox56 <zerox56@live.nl>
2025-04-16 22:36:40 +0200

    Add remaining CB functions

--- GameboyEmulator/src/cpu/modules/cpu_bit_shift.cpp
+++ GameboyEmulator/src/cpu/modules/cpu_bit_shift.cpp
@@ -8,8 +8,41 @@ namespace CPUBitShift {
 	}
 
 	CPU::CounterAction ExecuteBitShift(CPU& cpu, CPU::Instruction instruction) {
-		if (instruction.opcode >= 0x00 && instruction.opcode < 0x08) {
+		if (instruction.opcode >= 0x00 && instruction.opcode <= 0x07) {
 			RLC_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x08 && instruction.opcode <= 0x0F) {
+			RRC_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x10 && instruction.opcode <= 0x17) {
+			RL_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x18 && instruction.opcode <= 0x1F) {
+			SLA_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x20 && instruction.opcode <= 0x27) {
+			SRA_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x28 && instruction.opcode <= 0x2F) {
+			RR_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x30 && instruction.opcode <= 0x37) {
+			SWAP_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x38 && instruction.opcode <= 0x3F) {
+			SRL_R(cpu, instruction);
+		}
+		else if (instruction.opcode >= 0x40 && instruction.opcode <= 0x7F) {
+			uint8_t bit = (instruction.opcode >> 3) & 0b111;
+			BIT_B_R(cpu, instruction, bit);
+		}
+		else if (instruction.opcode >= 0x80 && instruction.opcode <= 0xBF) {
+			uint8_t bit = (instruction.opcode >> 3) & 0b111;
+			RES_B_R(cpu, instruction, bit);
+		}
+		else if (instruction.opcode >= 0xC0 && instruction.opcode <= 0xFF) {
+			uint8_t bit = (instruction.opcode >> 3) & 0b111;
+			SET_B_R(cpu, instruction, bit);
 		}
 
 		return CPU::CounterAction::Advance;
@@ -19,11 +52,128 @@ namespace CPUBitShift {
 		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
 
 		bool shiftedBit = BitUtils::GetMSB(value);
-		value <<= 1;
+		value = (value << 1) | shiftedBit;
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
 
 		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
-			CPUHelper::FlagsType::False, CPUHelper::FlagsType::BasedOnValue);
+			CPUHelper::FlagsType::False, C);
 
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void RRC_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetLSB(value);
+		value = (value >> 1) | (shiftedBit << 7);
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, shiftedBit, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void RL_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetMSB(value);
+		value = (value << 1) | cpu.state.FC;
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void RR_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetLSB(value);
+		value = (value >> 1) | (cpu.state.FC << 7);
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void SLA_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetMSB(value);
+		value = (value << 1) | 0;
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void SRA_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetLSB(value);
+		value = (value >> 1) | (value & 0x80);
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void SWAP_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		value = (value << 4) | (value >> 4);
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, CPUHelper::FlagsType::False);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void SRL_R(CPU& cpu, CPU::Instruction instruction) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+
+		bool shiftedBit = BitUtils::GetLSB(value);
+		value = value >> 1;
+
+		CPUHelper::FlagsType C = shiftedBit ? CPUHelper::FlagsType::True : CPUHelper::FlagsType::False;
+
+		CPUHelper::UpdateFlags(cpu, value, value, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::False, C);
+
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void BIT_B_R(CPU& cpu, CPU::Instruction instruction, uint8_t bit) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+		uint8_t bitValue = (value >> bit) & 1;
+
+		CPUHelper::UpdateFlags(cpu, bitValue, bitValue, CPUHelper::FlagsType::ValueZero, CPUHelper::FlagsType::False,
+			CPUHelper::FlagsType::True, CPUHelper::FlagsType::None);
+	}
+
+	void SET_B_R(CPU& cpu, CPU::Instruction instruction, uint8_t bit) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+		value |= (1 << bit);
+		CPUHelper::SetRegisterValue(cpu, instruction, value);
+	}
+
+	void RES_B_R(CPU& cpu, CPU::Instruction instruction, uint8_t bit) {
+		uint8_t value = CPUHelper::GetRegisterValue(cpu, instruction);
+		value &= ~(1 << bit);
 		CPUHelper::SetRegisterValue(cpu, instruction, value);
 	}
 }
--- GameboyEmulator/src/cpu/modules/cpu_bit_shift.h
+++ GameboyEmulator/src/cpu/modules/cpu_bit_shift.h
@@ -6,4 +6,14 @@ namespace CPUBitShift {
 	CPU::CounterAction ExecuteBitShift(CPU&, CPU::Instruction);
 
 	void RLC_R(CPU&, CPU::Instruction);
+	void RRC_R(CPU&, CPU::Instruction);
+	void RL_R(CPU&, CPU::Instruction);
+	void RR_R(CPU&, CPU::Instruction);
+	void SLA_R(CPU&, CPU::Instruction);
+	void SRA_R(CPU&, CPU::Instruction);
+	void SWAP_R(CPU&, CPU::Instruction);
+	void SRL_R(CPU&, CPU::Instruction);
+	void BIT_B_R(CPU&, CPU::Instruction, uint8_t bit);
+	void RES_B_R(CPU&, CPU::Instruction, uint8_t bit);
+	void SET_B_R(CPU&, CPU::Instruction, uint8_t bit);
 };
