8557b49baf4eb6757b1afd474508fdd2e0e47d16
zerox56 <zerox56@live.nl>
2025-04-19 01:35:18 +0200

    Add draw sprites function

--- GameboyEmulator/src/ppu/ppu.cpp
+++ GameboyEmulator/src/ppu/ppu.cpp
@@ -1,4 +1,4 @@
-#include "ppu.h"
+﻿#include "ppu.h"
 
 PPU::PPU() {
 	display.resize(screenWidth * screenHeigth);
@@ -11,6 +11,7 @@ void PPU::Update(std::vector<uint8_t>& memory, uint8_t cycles) {
 	UpdateSTAT(memory);	
 	FetchBackground(memory);
 	FetchWindow(memory);
+	DrawSprites(memory);
 }
 
 void PPU::UpdateMode(std::vector<uint8_t>& memory) {
@@ -60,7 +61,7 @@ void PPU::UpdateMode(std::vector<uint8_t>& memory) {
 void PPU::UpdateSTAT(std::vector<uint8_t>& memory) {
 	uint8_t STAT = memory[STATAddress];
 	if (currentMode == PPUMode::HBlank || currentMode == PPUMode::OAMSearch) {
-		STAT = (STAT & 0b11111100) | static_cast<uint8_t>(currentMode);  // Keep bits 2�7
+		STAT = (STAT & 0b11111100) | static_cast<uint8_t>(currentMode);  // Keep bits 2–7
 	}
 	if (memory[LYAddress] == memory[LYCAddress]) {
 		STAT |= 0x04; // Set LYC=LY flag
@@ -113,6 +114,21 @@ uint8_t PPU::GetColorValue(std::vector<uint8_t>& memory, uint8_t tileDataAddress
 	return (memory[BGPAddress] >> (colorId * 2)) & 3;;
 }
 
+uint8_t PPU::GetSpriteColorValue(std::vector<uint8_t>& memory, uint8_t tileDataAddress, uint8_t xBit, bool useOBP1) {
+	uint8_t lowBitPlane = memory[tileDataAddress];
+	uint8_t highBitPlane = memory[tileDataAddress + 1];
+
+	uint8_t low = (lowBitPlane >> xBit) & 1;
+	uint8_t high = (highBitPlane >> xBit) & 1;
+
+	uint8_t colorId = (high << 1) | low;
+
+	if (colorId == 0) return 0;
+
+	uint16_t paletteAddress = useOBP1 ? OBP1StartAddress : OBP0StartAddress;
+	return (memory[paletteAddress] >> (colorId * 2)) & 3;
+}
+
 void PPU::FetchBackground(std::vector<uint8_t>& memory) {
 	uint8_t SCX = memory[SCXAddress];
 	uint8_t SCY = memory[SCYAddress];
@@ -139,17 +155,13 @@ void PPU::FetchBackground(std::vector<uint8_t>& memory) {
 void PPU::FetchWindow(std::vector<uint8_t>& memory) {
 	uint8_t LCDC = memory[LCDCAddress];
 	bool renderWindow = (LCDC >> 5) & 1;
-	if (!renderWindow) {
-		return;
-	}
+	if (!renderWindow) return;
 
 	uint8_t WX = memory[WXAddress];
 	uint8_t WY = memory[WYAddress];
 	uint8_t LY = memory[LYAddress];
 
-	if (LY < WY) {
-		return;
-	}
+	if (LY < WY) return;
 
 	bool useTileMap1 = (LCDC >> 6) & 1;
 	uint16_t tileMapBaseAddress = useTileMap1 ? tileMap1Start : tileMap0Start;
@@ -169,5 +181,59 @@ void PPU::FetchWindow(std::vector<uint8_t>& memory) {
 		uint8_t xBit = 7 - pixelInTile;
 		uint8_t colorValue = GetColorValue(memory, tileDataAddress, xBit);
 		display[LY * screenWidth + x] = colorValue;
+	}
+}
+
+void PPU::DrawSprites(std::vector<uint8_t>& memory) {
+	uint8_t LCDC = memory[LCDCAddress];
+	uint8_t LY = memory[LYAddress];
+	bool objDoubleHeight = (LCDC >> 2) & 1;
+	std::vector<Sprite> visibleSprites;
+
+	for (uint8_t i = 0; i < 40; i++) {
+		uint16_t oamAddress = OAMStartAddress + (i * spriteSize);
+		Sprite sprite;
+		sprite.y = memory[oamAddress] - 16;
+		sprite.x = memory[oamAddress + 1] - 8;
+		sprite.tileId = memory[oamAddress + 2];
+		sprite.attributes = memory[oamAddress + 3];
+
+		if (objDoubleHeight) {
+			sprite.tileId &= 0xFE;
+		}
+
+		if (LY >= sprite.y && LY < sprite.y + 16) {
+			visibleSprites.push_back(sprite);
+		}
+
+		if (visibleSprites.size() == 10) break;
+	}
+
+
+	for (uint8_t spriteIndex = 0; spriteIndex < visibleSprites.size(); spriteIndex++) {
+		Sprite sprite = visibleSprites[spriteIndex];
+		uint8_t yInTile = LY - sprite.y;
+		bool useOBP1 = sprite.attributes & 0x10;
+
+		bool isTileUnsigned = (LCDC >> 4) & 1;
+		uint16_t tileBaseAddress = isTileUnsigned ? unsignedTilesStart : signedTilesStart;
+
+		uint16_t tileAddress = tileBaseAddress + sprite.tileId * 16;
+		uint16_t tileDataAddress = tileAddress + yInTile * 2;
+
+		for (uint8_t x = 0; x < 8; x++) {
+			uint8_t xPixel = sprite.x + x;
+			if (xPixel >= 160) continue;
+
+			uint8_t xBit = 7 - x;
+			uint8_t colorValue = GetSpriteColorValue(memory, tileDataAddress, xBit, useOBP1);
+
+			if (colorValue == 0) continue;
+
+			bool backgroundPriority = sprite.attributes & 0x80;
+			if (backgroundPriority && display[LY * 160 + xPixel] != 0) continue;
+
+			display[LY * 160 + xPixel] = colorValue;
+		}
 	}
 }
--- GameboyEmulator/src/ppu/ppu.h
+++ GameboyEmulator/src/ppu/ppu.h
@@ -22,9 +22,9 @@ private:
 	PPUMode currentMode = PPUMode::HBlank;
 
 	// LCD
-	uint8_t LCDCAddress = 0xFF40;
-	uint8_t LYAddress = 0xFF44;
-	uint8_t LYCAddress = 0xFF45;
+	uint16_t LCDCAddress = 0xFF40;
+	uint16_t LYAddress = 0xFF44;
+	uint16_t LYCAddress = 0xFF45;
 	uint16_t tileMap0Start = 0x9800;
 	uint16_t tileMap1Start = 0x9C00;
 	uint16_t unsignedTilesStart = 0x8000;
@@ -36,18 +36,31 @@ private:
 
 	// BG related
 	// Scroll offset
-	uint8_t SCYAddress = 0xFF42;
-	uint8_t SCXAddress = 0xFF43;
+	uint16_t SCYAddress = 0xFF42;
+	uint16_t SCXAddress = 0xFF43;
 	// Background palette
-	uint8_t BGPAddress = 0xFF47;
+	uint16_t BGPAddress = 0xFF47;
 
 	// Window related
-	uint8_t WXAddress = 0xFF4B;
-	uint8_t WYAddress = 0xFF4A;
+	uint16_t WXAddress = 0xFF4B;
+	uint16_t WYAddress = 0xFF4A;
 
 	// Interrupt Flag
-	uint8_t IFAddress = 0xFF0F;
-	uint8_t STATAddress = 0xFF41;
+	uint16_t IFAddress = 0xFF0F;
+	uint16_t STATAddress = 0xFF41;
+
+	// Sprites
+	uint16_t OAMStartAddress = 0xFE00;
+	// Palettes
+	uint16_t OBP0StartAddress = 0xFF48;
+	uint16_t OBP1StartAddress = 0xFF49;
+	struct Sprite {
+		uint8_t y;
+		uint8_t x;
+		uint16_t tileId;
+		uint8_t attributes;
+	};
+	uint8_t spriteSize = 4;
 
 	void UpdateMode(std::vector<uint8_t>& memory);
 	void UpdateSTAT(std::vector<uint8_t>& memory);
@@ -55,6 +68,9 @@ private:
 	uint8_t GetTileDataAddress(std::vector<uint8_t>& memory, uint8_t column, uint8_t row, uint8_t pixelInTile,
 		uint16_t tileMapBaseAddress, bool isTileUnsigned);
 	uint8_t GetColorValue(std::vector<uint8_t>& memory, uint8_t tileDataAddress, uint8_t xBit);
+	uint8_t GetSpriteColorValue(std::vector<uint8_t>& memory, uint8_t tileDataAddress, uint8_t xBit, bool useOBP1);
 	void FetchBackground(std::vector<uint8_t>& memory);
 	void FetchWindow(std::vector<uint8_t>& memory);
+
+	void DrawSprites(std::vector<uint8_t>& memor);
 };
