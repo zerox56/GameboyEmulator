95ed39b1fd4df73fc03efbf3048f3c46cc3bf873
zerox56 <zerox56@live.nl>
2025-04-19 02:21:00 +0200

    Add SDL rendering

--- GameboyEmulator/src/sdl/sdl_renderer.cpp
+++ GameboyEmulator/src/sdl/sdl_renderer.cpp
@@ -1,11 +1,27 @@
-#include "sdl_renderer.h"
+﻿#include "sdl_renderer.h"
 
 void SDLRenderer::Initialize() {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	window = SDL_CreateWindow("Gameboy Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		160 * scale, 144 * scale, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, 160, 144);
 }
 
 void SDLRenderer::Render(std::vector<uint8_t>& display) {
+	SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch);
 
+	for (int y = 0; y < 144; y++) {
+		for (int x = 0; x < 160; x++) {
+			uint8_t pixel = display[y * 160 + x];
+			SDL_Color color = colors[pixel];
+			pixels[y * (pitch / 4) + x] = (color.r << 16) | (color.g << 8) | color.b;;
+		}
+	}
+
+	SDL_UnlockTexture(texture);
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+	SDL_RenderPresent(renderer);
 }
--- GameboyEmulator/src/sdl/sdl_renderer.h
+++ GameboyEmulator/src/sdl/sdl_renderer.h
@@ -10,11 +10,17 @@ public:
     void Render(std::vector<uint8_t>& display);
 private:
     static constexpr uint8_t scale = 10;
-    static constexpr SDL_Color whiteColor = { 155, 188, 15, 255 };
-    static constexpr SDL_Color lightGrayColor = { 139, 172, 15, 255 };
-    static constexpr SDL_Color darkGrayColor = { 48, 98, 48, 255 };
-    static constexpr SDL_Color blackColor = { 15, 56,15, 255 };
+
+    SDL_Color whiteColor = { 155, 188, 15, 255 };
+    SDL_Color lightGrayColor = { 139, 172, 15, 255 };
+    SDL_Color darkGrayColor = { 48, 98, 48, 255 };
+    SDL_Color blackColor = { 15, 56,15, 255 };
+    std::vector<SDL_Color> colors = { whiteColor, lightGrayColor, darkGrayColor, blackColor };
 
     SDL_Window* window;
     SDL_Renderer* renderer;
+    SDL_Texture* texture;
+
+    uint32_t* pixels = nullptr;
+    int pitch = 0;
 };
